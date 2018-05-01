be4e5d0177d2b364821bb5a966f27fc88c029e1d
maatthias <maatthias@gmail.com>
2018-05-01 07:27:59 -0400

    Add files via upload

---
+++ DownloadAttachments/dyn365.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace DownloadAttachments
+{
+    partial class dyn365
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn365Connect = new System.Windows.Forms.Button();
+            this.discoverySvcUrl = new System.Windows.Forms.TextBox();
+            this.lblDiscoverySvcUrl = new System.Windows.Forms.Label();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblOrg = new System.Windows.Forms.Label();
+            this.txtOrg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn365Connect
+            // 
+            this.btn365Connect.Location = new System.Drawing.Point(195, 157);
+            this.btn365Connect.Name = "btn365Connect";
+            this.btn365Connect.Size = new System.Drawing.Size(75, 23);
+            this.btn365Connect.TabIndex = 0;
+            this.btn365Connect.Text = "connect";
+            this.btn365Connect.UseVisualStyleBackColor = true;
+            this.btn365Connect.Click += new System.EventHandler(this.btn365Connect_Click);
+            // 
+            // discoverySvcUrl
+            // 
+            this.discoverySvcUrl.Location = new System.Drawing.Point(170, 37);
+            this.discoverySvcUrl.Name = "discoverySvcUrl";
+            this.discoverySvcUrl.Size = new System.Drawing.Size(506, 20);
+            this.discoverySvcUrl.TabIndex = 1;
+            this.discoverySvcUrl.Text = "https://disco.crm.dynamics.com/XRMServices/2011/Discovery.svc";
+            // 
+            // lblDiscoverySvcUrl
+            // 
+            this.lblDiscoverySvcUrl.AutoSize = true;
+            this.lblDiscoverySvcUrl.Location = new System.Drawing.Point(46, 37);
+            this.lblDiscoverySvcUrl.Name = "lblDiscoverySvcUrl";
+            this.lblDiscoverySvcUrl.Size = new System.Drawing.Size(118, 13);
+            this.lblDiscoverySvcUrl.TabIndex = 2;
+            this.lblDiscoverySvcUrl.Text = "Discovery Service URL";
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(46, 63);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(53, 13);
+            this.lblUserName.TabIndex = 4;
+            this.lblUserName.Text = "username";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(170, 63);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(100, 20);
+            this.username.TabIndex = 3;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(46, 89);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(52, 13);
+            this.lblPassword.TabIndex = 6;
+            this.lblPassword.Text = "password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(170, 89);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // lblOrg
+            // 
+            this.lblOrg.AutoSize = true;
+            this.lblOrg.Location = new System.Drawing.Point(46, 115);
+            this.lblOrg.Name = "lblOrg";
+            this.lblOrg.Size = new System.Drawing.Size(86, 13);
+            this.lblOrg.TabIndex = 8;
+            this.lblOrg.Text = "unique org name";
+            // 
+            // txtOrg
+            // 
+            this.txtOrg.Location = new System.Drawing.Point(170, 115);
+            this.txtOrg.Name = "txtOrg";
+            this.txtOrg.Size = new System.Drawing.Size(100, 20);
+            this.txtOrg.TabIndex = 7;
+
+            // 
+            // dyn365
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 279);
+            this.Controls.Add(this.lblOrg);
+            this.Controls.Add(this.txtOrg);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.lblDiscoverySvcUrl);
+            this.Controls.Add(this.discoverySvcUrl);
+            this.Controls.Add(this.btn365Connect);
+            this.Name = "dyn365";
+            this.Text = "dyn365";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn365Connect;
+        private System.Windows.Forms.TextBox discoverySvcUrl;
+        private System.Windows.Forms.Label lblDiscoverySvcUrl;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblOrg;
+        private System.Windows.Forms.TextBox txtOrg;
+    }
+}
---
+++ DownloadAttachments/dyn365.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Reflection;
+
+// These namespaces are found in the Microsoft.Xrm.Sdk.dll assembly
+// located in the SDK\bin folder of the SDK download.
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Tooling.Connector;
+using Microsoft.Xrm.Sdk.Discovery;
+using Microsoft.Crm.Sdk.Messages;
+
+namespace DownloadAttachments
+{
+    public partial class dyn365 : Form
+    {
+
+        // To get discovery service address and organization unique name, 
+        // Sign in to your CRM org and click Settings, Customization, Developer Resources.
+        // On Developer Resource page, find the discovery service address under Service Endpoints and organization unique name under Your Organization Information.
+        private String _discoveryServiceAddress = String.Empty;
+        private String _organizationUniqueName = String.Empty;
+        // Provide your user name and password.
+        private String _userName = String.Empty;
+        private String _password = String.Empty;
+
+        // Provide domain name for the On-Premises org.
+        private String _domain = String.Empty;
+
+        public dyn365()
+        {
+            InitializeComponent();
+        }
+
+        private void btn365Connect_Click(object sender, EventArgs e)
+        {
+            _discoveryServiceAddress = discoverySvcUrl.Text;
+            _organizationUniqueName = txtOrg.Text;
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            IServiceManagement<IDiscoveryService> serviceManagement =
+            ServiceConfigurationFactory.CreateManagement<IDiscoveryService>(
+            new Uri(_discoveryServiceAddress));
+            AuthenticationProviderType endpointType = serviceManagement.AuthenticationType;
+
+            // Set the credentials.
+            AuthenticationCredentials authCredentials = GetCredentials(serviceManagement, endpointType);
+
+
+            String organizationUri = String.Empty;
+            // Get the discovery service proxy.
+            using (DiscoveryServiceProxy discoveryProxy =
+                GetProxy<IDiscoveryService, DiscoveryServiceProxy>(serviceManagement, authCredentials))
+            {
+                // Obtain organization information from the Discovery service. 
+                if (discoveryProxy != null)
+                {
+                    // Obtain information about the organizations that the system user belongs to.
+                    OrganizationDetailCollection orgs = DiscoverOrganizations(discoveryProxy);
+                    // Obtains the Web address (Uri) of the target organization.
+                    organizationUri = FindOrganization(_organizationUniqueName,
+                        orgs.ToArray()).Endpoints[EndpointType.OrganizationService];
+
+                }
+            }
+
+
+            if (!String.IsNullOrWhiteSpace(organizationUri))
+            {
+                IServiceManagement<IOrganizationService> orgServiceManagement =
+                    ServiceConfigurationFactory.CreateManagement<IOrganizationService>(
+                    new Uri(organizationUri));
+
+                // Set the credentials.
+                AuthenticationCredentials credentials = GetCredentials(orgServiceManagement, endpointType);
+
+                // Get the organization service proxy.
+                using (OrganizationServiceProxy organizationProxy =
+                    GetProxy<IOrganizationService, OrganizationServiceProxy>(orgServiceManagement, credentials))
+                {
+                    // This statement is required to enable early-bound type support.
+                    organizationProxy.EnableProxyTypes();
+
+                    // Now make an SDK call with the organization service proxy.
+                    // Display information about the logged on user.
+                    Guid userid = ((WhoAmIResponse)organizationProxy.Execute(
+                        new WhoAmIRequest())).UserId;
+                    IOrganizationService service = null;
+                    service = (IOrganizationService)organizationProxy;
+                    DownloadAttachment obj1 = new DownloadAttachment(service);
+                    obj1.Show();
+                    this.Hide();
+                    //SystemUser systemUser = organizationProxy.Retrieve("systemuser", userid, new ColumnSet(new string[] { "firstname", "lastname" })).ToEntity<SystemUser>();
+                    //Console.WriteLine("Logged on user is {0} {1}.", systemUser.FirstName, systemUser.LastName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Obtain the AuthenticationCredentials based on AuthenticationProviderType.
+        /// </summary>
+        /// <param name="service">A service management object.</param>
+        /// <param name="endpointType">An AuthenticationProviderType of the CRM environment.</param>
+        /// <returns>Get filled credentials.</returns>
+        private AuthenticationCredentials GetCredentials<TService>(IServiceManagement<TService> service, AuthenticationProviderType endpointType)
+        {
+            AuthenticationCredentials authCredentials = new AuthenticationCredentials();
+            _userName = username.Text;
+            _password = txtPassword.Text;
+            
+            switch (endpointType)
+            {
+                case AuthenticationProviderType.ActiveDirectory:
+                    authCredentials.ClientCredentials.Windows.ClientCredential =
+                        new System.Net.NetworkCredential(_userName,
+                            _password,
+                            _domain);
+                    break;
+                case AuthenticationProviderType.LiveId:
+                    authCredentials.ClientCredentials.UserName.UserName = _userName;
+                    authCredentials.ClientCredentials.UserName.Password = _password;
+                    authCredentials.SupportingCredentials = new AuthenticationCredentials();
+                    //authCredentials.SupportingCredentials.ClientCredentials = Microsoft.Crm.Services.Utility.DeviceIdManager.LoadOrRegisterDevice();
+                    break;
+                default: // For Federated and OnlineFederated environments.                    
+                    authCredentials.ClientCredentials.UserName.UserName = _userName;
+                    authCredentials.ClientCredentials.UserName.Password = _password;
+                    // For OnlineFederated single-sign on, you could just use current UserPrincipalName instead of passing user name and password.
+                    // authCredentials.UserPrincipalName = UserPrincipal.Current.UserPrincipalName;  // Windows Kerberos
+
+                    // The service is configured for User Id authentication, but the user might provide Microsoft
+                    // account credentials. If so, the supporting credentials must contain the device credentials.
+                    if (endpointType == AuthenticationProviderType.OnlineFederation)
+                    {
+                        IdentityProvider provider = service.GetIdentityProvider(authCredentials.ClientCredentials.UserName.UserName);
+                        if (provider != null && provider.IdentityProviderType == IdentityProviderType.LiveId)
+                        {
+                            authCredentials.SupportingCredentials = new AuthenticationCredentials();
+                            //authCredentials.SupportingCredentials.ClientCredentials = Microsoft.Crm.Services.Utility.DeviceIdManager.LoadOrRegisterDevice();
+                            authCredentials.SupportingCredentials.ClientCredentials = DeviceIdManager.LoadOrRegisterDevice();
+                        }
+                    }
+
+                    break;
+            }
+
+            return authCredentials;
+        }
+
+        /// <summary>
+        /// Discovers the organizations that the calling user belongs to.
+        /// </summary>
+        /// <param name="service">A Discovery service proxy instance.</param>
+        /// <returns>Array containing detailed information on each organization that 
+        /// the user belongs to.</returns>
+        public OrganizationDetailCollection DiscoverOrganizations(
+            IDiscoveryService service)
+        {
+            if (service == null) throw new ArgumentNullException("service");
+            RetrieveOrganizationsRequest orgRequest = new RetrieveOrganizationsRequest();
+            RetrieveOrganizationsResponse orgResponse =
+                (RetrieveOrganizationsResponse)service.Execute(orgRequest);
+
+            return orgResponse.Details;
+        }
+
+        /// <summary>
+        /// Finds a specific organization detail in the array of organization details
+        /// returned from the Discovery service.
+        /// </summary>
+        /// <param name="orgUniqueName">The unique name of the organization to find.</param>
+        /// <param name="orgDetails">Array of organization detail object returned from the discovery service.</param>
+        /// <returns>Organization details or null if the organization was not found.</returns>
+        /// <seealso cref="DiscoveryOrganizations"/>
+        public OrganizationDetail FindOrganization(string orgUniqueName,
+            OrganizationDetail[] orgDetails)
+        {
+            if (String.IsNullOrWhiteSpace(orgUniqueName))
+                throw new ArgumentNullException("orgUniqueName");
+            if (orgDetails == null)
+                throw new ArgumentNullException("orgDetails");
+            OrganizationDetail orgDetail = null;
+
+            foreach (OrganizationDetail detail in orgDetails)
+            {
+                if (String.Compare(detail.UniqueName, orgUniqueName,
+                    StringComparison.InvariantCultureIgnoreCase) == 0)
+                {
+                    orgDetail = detail;
+                    break;
+                }
+            }
+            return orgDetail;
+        }
+
+        /// <summary>
+        /// Generic method to obtain discovery/organization service proxy instance.
+        /// </summary>
+        /// <typeparam name="TService">
+        /// Set IDiscoveryService or IOrganizationService type to request respective service proxy instance.
+        /// </typeparam>
+        /// <typeparam name="TProxy">
+        /// Set the return type to either DiscoveryServiceProxy or OrganizationServiceProxy type based on TService type.
+        /// </typeparam>
+        /// <param name="serviceManagement">An instance of IServiceManagement</param>
+        /// <param name="authCredentials">The user's Microsoft Dynamics CRM logon credentials.</param>
+        /// <returns></returns>
+        private TProxy GetProxy<TService, TProxy>(
+            IServiceManagement<TService> serviceManagement,
+            AuthenticationCredentials authCredentials)
+            where TService : class
+            where TProxy : ServiceProxy<TService>
+        {
+            Type classType = typeof(TProxy);
+
+            if (serviceManagement.AuthenticationType !=
+                AuthenticationProviderType.ActiveDirectory)
+            {
+                AuthenticationCredentials tokenCredentials =
+                    serviceManagement.Authenticate(authCredentials);
+                // Obtain discovery/organization service proxy for Federated, LiveId and OnlineFederated environments. 
+                // Instantiate a new class of type using the 2 parameter constructor of type IServiceManagement and SecurityTokenResponse.
+                return (TProxy)classType
+                    .GetConstructor(new Type[] { typeof(IServiceManagement<TService>), typeof(SecurityTokenResponse) })
+                    .Invoke(new object[] { serviceManagement, tokenCredentials.SecurityTokenResponse });
+            }
+
+            // Obtain discovery/organization service proxy for ActiveDirectory environment.
+            // Instantiate a new class of type using the 2 parameter constructor of type IServiceManagement and ClientCredentials.
+            return (TProxy)classType
+                .GetConstructor(new Type[] { typeof(IServiceManagement<TService>), typeof(ClientCredentials) })
+                .Invoke(new object[] { serviceManagement, authCredentials.ClientCredentials });
+        }
+        
+    }
+}
